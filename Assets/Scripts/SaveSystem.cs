faf5a82d08d6620cf27570b3b110164ab023128d
Florian <florian0205@gmx.de>
2020-08-05 18:31:31 +0200

    Finger weg Sky

---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SaveSystem : MonoBehaviour
+{
+    public void SaveData()
+    {
+        
+    }
+}
