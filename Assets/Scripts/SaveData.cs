4f8b51873d93050c986d1e7b1759ed9b44119528
Florian <florian0205@gmx.de>
2020-08-05 19:45:27 +0200

    Savesystem added

--- Assets/Scripts/SaveData.cs
+++ Assets/Scripts/SaveData.cs
@@ -2,17 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SaveData : MonoBehaviour
+public class SaveData
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //public "int" "INTEGER"
 
-    // Update is called once per frame
-    void Update()
+
+    //public SaveData("Class""bsp",)
+    public SaveData()
     {
-        
+        //"INTERGER" = "bsp.health"
     }
 }
--- Assets/Scripts/SaveSystem.cs
+++ Assets/Scripts/SaveSystem.cs
@@ -2,11 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
-public class SaveSystem : MonoBehaviour
+public static class SaveSystem
 {
-    public void SaveData()
+    //public static void SaveData("Class""bsp",)
+    public static void SaveData()
     {
-        
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/data.jeff";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        SaveData data = new SaveData();
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static SaveData LoadData()
+    {
+        string path = Application.persistentDataPath + "/data.jeff";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            SaveData data = formatter.Deserialize(stream) as SaveData;
+            stream.Close();
+
+            return data;
+        }
+        else
+        {
+            Debug.LogError("Savefile not found in " + path);
+            return null;
+        }
+
     }
 }
