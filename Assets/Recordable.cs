453ac9e99950e8198757f57cdc5ad962830fa983
SkyScroller <60900223+SkyScroller@users.noreply.github.com>
2020-08-06 17:51:00 +0200

    Rewind 2020

---
+++ Assets/Recordable.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recordable : MonoBehaviour
+{
+    public bool idfc = false;
+    public bool wasRecorded = false;
+    public List<Vector3> locRot;
+    public bool rewind;
+
+    public void SetWasRecored(bool value)
+    {
+        wasRecorded = value;
+
+        if(value == true && this.GetComponent<Rigidbody2D>() != null)
+        {
+            this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        }
+        else if(this.GetComponent<Rigidbody2D>() != null)
+        {
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(rewind == true && locRot.Count >= 1)
+        {
+            this.transform.position = new Vector3(locRot[0].x, locRot[0].y, this.transform.position.z);
+            this.transform.eulerAngles = new Vector3(0,0, locRot[0].z);
+            locRot.RemoveAt(0);
+        }else if(rewind)
+        {
+            rewind = false;
+            if (this.GetComponent<Rigidbody2D>() != null)
+            {
+                this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            }
+        }
+    }
+
+    public void Rewind()
+    {
+        rewind = true;
+    }
+
+}
